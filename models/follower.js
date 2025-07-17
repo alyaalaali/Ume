b0926a14ea147a545f1a1c8f194cceef141d2967
Ali Hasan Habib <alimodffs2@gmail.com>
2025-07-17 22:17:54 +0300

    add schema structure

---
+++ models/follower.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const { ref } = require("process")
+
+const followerSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  followingId: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  followersId: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+})
+
+const Follower = mongoose.model("Follower", followerSchema)
+
+module.exports = Follower
