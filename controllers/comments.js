e4ec1c7c2ed8bf9573e6fd89ae72244ad1c04ec7
0yousif <ysoofee1@gmail.com>
2025-07-18 22:10:11 +0300

    comments deletion API was added, alongside edits to the comment creation API

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,24 +1,43 @@
 const Comment = require("./../models/comment")
 const Post = require("./../models/post")
 
-
-
-exports.comments_index_get = async (req,res)=>{
-  const post_commnets = await Comment.find({postId: req.params.postId})
+exports.comments_index_get = async (req, res) => {
+  const post_commnets = await Comment.find({ postId: req.params.postId })
   res.send(post_commnets)
 }
 
-exports.comments_create_post = async (req,res)=>{
+exports.comments_create_post = async (req, res) => {
   req.body.postId = req.params.postId
-  req.body.userId = req.session.userId
-  await Comment.create(req.body)
+  req.body.userId = req.session.user._id
+  let newComment = await Comment.create(req.body)
+  
+  Post.findByIdAndUpdate(req.body.postId, {
+    $push: { comments: newComment._id },
+  })
 }
 
-exports.comments_update_put = async (req,res)=>{
-  const comment = awaitComment.findById(req.params.commentId)
+exports.comments_update_put = async (req, res) => {
+  const comment = await Comment.findById(req.params.commentId)
 
-  if (comment.userId.equals(req.session.user._id)){
+  if (comment.userId.equals(req.session.user._id)) {
     comment.set(req.body)
     await comment.save()
+    res.send("Comment created successfully")
+  } else {
+    res.send("ur not the comment owner")
+  }
+}
+
+exports.comments_delete_delete = async (req, res) => {
+  const comment = await Comment.findById(req.params.commentId)
+
+  if (comment.userId.equals(req.session.user._id)) {
+    await Post.findByIdAndUpdate(req.body.postId, {
+      $pull: { comments: comment._id },
+    })
+    await Post.findByIdAndDelete(req.body.postId)
+    res.send("The comment has been deleted successfully")
+  } else {
+    res.send("ur not the comment owner")
   }
 }
--- routes/comments.js
+++ routes/comments.js
@@ -4,9 +4,11 @@ const Comment = require("./../models/comment")
 
 
 
+
 router.get("/:postId", commentsCtrl.comments_index_get)
 router.post("/:postId", commentsCtrl.comments_create_post)
-router.PUT("/:commentId", commentsCtrl.comments_update_put)
+router.put("/:commentId", commentsCtrl.comments_update_put)
+router.delete("/:commentId", commentsCtrl.comments_delete_delete)
 
 
 
