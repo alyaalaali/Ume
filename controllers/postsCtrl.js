c59aa79bca8d6e7ae5d46aff4248e461ee798d8e
Ali Hasan Habib <alimodffs2@gmail.com>
2025-07-18 22:46:41 +0300

    create functionality done

--- controllers/postsCtrl.js
+++ controllers/postsCtrl.js
@@ -16,3 +16,26 @@ exports.post_index_get = async (req, res) => {
     res.status(500).json({ error: "Page not found" })
   }
 }
+
+exports.post_create_get = async (req, res) => {
+  try {
+    res.status(200).render("posts/new.ejs")
+  } catch (error) {
+    res.status(500).json({ error: "Failed to spawn new page" })
+  }
+}
+
+exports.post_create_post = async (req, res) => {
+  try {
+    const postData = {
+      photo: req.body.photo,
+      caption: req.body.caption,
+      userId: req.session.userId
+    }
+
+    await Post.create(postData)
+    res.redirect("/posts")
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create post" })
+  }
+}
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -5,5 +5,7 @@ const postCtrl = require("../controllers/postsCtrl.js")
 // Routes / call API's
 
 router.get("/", postCtrl.post_index_get)
+router.get("/new", postCtrl.post_create_get)
+router.post("/:userId", postCtrl.post_create_post)
 
 module.exports = router
