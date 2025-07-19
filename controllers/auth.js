3c2afc9e85e0508802ddd732136a4443e52b6ba6
Yara Waleed <yarawaleed292@gmail.com>
2025-07-19 19:08:17 +0300

    cleaned up the auth.js page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-
 const User = require("../models/user.js");
 
-const userInDatabase = await User.findOne({ username: req.body.username });
+//API's
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
+});
+
+router.post("/sign-up", async (req, res) => 
+  {const userInDatabase = await User.findOne({ username: req.body.username });
 if (userInDatabase) {
   return res.send("Username already taken.");
 }
+
 if (req.body.password !== req.body.confirmPassword) {
   return res.send("Password and Confirm Password must match");
 }
+});
 
 // password encryption - bcrypt
 const hashedPassword = bcrypt.hashSync(req.body.password, 10);
@@ -20,16 +27,29 @@ req.body.password = hashedPassword;
 const user = await User.create(req.body);
 res.send(`Thanks for signing up ${user.username}`);
 
-router.get("/sign-up", (req, res) => {
-  res.render("auth/sign-up.ejs");
-});
-
-router.post("/sign-up", async (req, res) => {
-  res.send("Form submission accepted!");
-});
 
 router.get("/sign-in", (req, res) => {
   res.render("auth/sign-in.ejs");
 });
+
+router.post("/sign-in", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (!userInDatabase) {
+    return res.send("Login failed. Please try again.");
+  }
+
+  const validPassword = bcrypt.compareSync(req.body.password, userInDatabase.password);
+  if (!validPassword) {
+    return res.send("Login failed. Please try again.");
+  }
+});
+
+//user exist and password matched 
+  req.session.user = {
+    username: userInDatabase.username, 
+    _id: userInDatabase._id
+  }
+  res.redirect("/")
+
 
 module.exports = router;
