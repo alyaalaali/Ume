363ac545a33e3df782e76d6ac6f5af3a46139003
alyaalaali <alya.alaali05@gmail.com>
2025-07-20 00:47:22 +0300

    created follow POST/DELETE requests

--- controllers/follows.js
+++ controllers/follows.js
@@ -1,3 +1,4 @@
+const User = require("./../models/user")
 const Follow = require("./../models/follower")
 
 exports.following_index_get = async (req, res) => {
@@ -23,5 +24,39 @@ exports.follower_index_get = async (req, res) => {
     res.send(followerList)
   } catch (error) {
     res.status(500).json({ error: "failed to get the followers list!" })
+  }
+}
+
+exports.follow_create_post = async (req, res) => {
+  try {
+    const followedUserId = req.params.userId
+    const user = req.session.user.userId
+
+    await Follow.findByIdAndUpdate(followedUserId, {
+      $push: { followersId: user },
+    })
+
+    await Follow.findByIdAndUpdate(user, {
+      $push: { followingId: followedUserId },
+    })
+  } catch {
+    res.status(500).json({ error: "failed to follow user!" })
+  }
+}
+
+exports.follow_delete_delete = async (req, res) => {
+  try {
+    const unfollowedUserId = req.params.userId
+    const user = req.session.user.userId
+
+    await Follow.findByIdAndUpdate(unfollowedUserId, {
+      $pull: { followersId: user },
+    })
+
+    await Follow.findByIdAndUpdate(user, {
+      $pull: { followingId: unfollowedUserId },
+    })
+  } catch {
+    res.status(500).json({ error: "failed to unfollow user!" })
   }
 }
