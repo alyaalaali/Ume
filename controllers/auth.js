9f84437c3258217b2cdf166f1e4f8d3ee6153841
Yara Waleed <yarawaleed292@gmail.com>
2025-07-19 18:50:46 +0300

    built a sign in page

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,4 +28,8 @@ router.post("/sign-up", async (req, res) => {
   res.send("Form submission accepted!");
 });
 
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in.ejs");
+});
+
 module.exports = router;
