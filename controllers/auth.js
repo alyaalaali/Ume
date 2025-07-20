f69e80e91027ba0948b13e5a7d3747174661285a
alyaalaali <alya.alaali05@gmail.com>
2025-07-20 14:13:29 +0300

    Changed Path

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,21 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const bcrypt = require('bcrypt')
-const User = require('../models/user.js')
+const bcrypt = require("bcrypt")
+const User = require("../models/user.js")
 
 //API's
 exports.auth_signup_get = async (req, res) => {
-  res.render('auth/sign-up.ejs')
+  res.render("auth/sign-up.ejs")
 }
 
 exports.auth_signup_post = async (req, res) => {
   const userInDatabase = await User.findOne({ username: req.body.username })
   if (userInDatabase) {
-    return res.send('Username already taken.')
+    return res.send("Username already taken.")
   }
 
   if (req.body.password !== req.body.confirmPassword) {
-    return res.send('Password and Confirm Password must match')
+    return res.send("Password and Confirm Password must match")
   }
 
   //bcryp
@@ -28,13 +28,13 @@ exports.auth_signup_post = async (req, res) => {
 }
 
 exports.auth_signin_get = async (req, res) => {
-  res.render('auth/sign-in.ejs')
+  res.render("auth/sign-in.ejs")
 }
 
 exports.auth_signin_post = async (req, res) => {
   const userInDatabase = await User.findOne({ username: req.body.username })
   if (!userInDatabase) {
-    return res.send('Login failed. Please try again.')
+    return res.send("Login failed. Please try again.")
   }
 
   const validPassword = bcrypt.compareSync(
@@ -42,37 +42,37 @@ exports.auth_signin_post = async (req, res) => {
     userInDatabase.password
   )
   if (!validPassword) {
-    return res.send('Login failed. Please try again.')
+    return res.send("Login failed. Please try again.")
   }
 
   //user exist and password matched
   req.session.user = {
     username: userInDatabase.username,
-    _id: userInDatabase._id
+    _id: userInDatabase._id,
   }
-  res.redirect('/')
+  res.render("index.ejs")
 }
 
 exports.auth_updatePassword = async (req, res) => {
   try {
     const user = await User.findById(req.params.id)
     if (!user) {
-      return res.send('No user with that ID exists!')
+      return res.send("No user with that ID exists!")
     }
     const validPassword = bcrypt.compareSync(
       req.body.oldPassword,
       user.password
     )
     if (!validPassword) {
-      return res.send('Your old password was not correct! Please try again.')
+      return res.send("Your old password was not correct! Please try again.")
     }
     if (req.body.newPassword !== req.body.confirmPassword) {
-      return res.send('Password and Confirm Password must match')
+      return res.send("Password and Confirm Password must match")
     }
     const hashedPassword = bcrypt.hashSync(req.body.newPassword, 12)
     user.password = hashedPassword
     await user.save()
-    res.render('./auth/confpas.ejs', { user })
+    res.render("./auth/confpas.ejs", { user })
   } catch (error) {
     console.error(
       "An error has occurred updating a user's password!",
@@ -83,5 +83,5 @@ exports.auth_updatePassword = async (req, res) => {
 
 exports.auth_signout_get = (req, res) => {
   req.session.destroy()
-  res.redirect('/')
+  res.redirect("/")
 }
