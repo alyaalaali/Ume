327066f65dae31f223923009d59c56791e66a000
0yousif <ysoofee1@gmail.com>
2025-07-22 12:15:42 +0300

    working on the dynamic comments page

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,22 +2,26 @@ const { post } = require("../routes/comments")
 const Comment = require("./../models/comment")
 const Post = require("./../models/post")
 
-
 exports.comment_index_get = async (req, res) => {
-  const postComments = await Comment.find({ postId: req.params.postId }).populate("userId")
-  console.log("post comments", postCommentsx``)
-  res.render("./comments/index.ejs", {pageName: "Comments", postComments})
+  const postComments = await Comment.find({ postId: req.params.postId })
 
+  res.render("./comments/index.ejs", {
+    pageName: "Comments",
+    postComments,
+    currentPostId: req.params.postId,
+  })
 }
 
 exports.comment_create_post = async (req, res) => {
   req.body.postId = req.params.postId
-  req.body.userId = req.session.user.userId
+  req.body.userId = req.session.user._id
   const newComment = await Comment.create(req.body)
 
   await Post.findByIdAndUpdate(newComment.postId, {
     $push: { comments: newComment._id },
   })
+
+  res.redirect(`/comments/${req.params.postId}`)
 }
 
 exports.comment_delete_delete = async (req, res) => {
